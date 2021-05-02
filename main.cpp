64176099056cfb8b75ad5317a1f912079d19843f
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-05-02 22:46:18 +0100

    usage - hook-up event for move left

--- main.cpp
+++ main.cpp
@@ -10,15 +10,27 @@ struct display_console_t : public display_t {
 int main(int argc, char** argv) {
   initscr(); // start curses mode
   curs_set(0); // hide cursor
+  cbreak(); // line buffering disabled (respects Ctrl-C to quit)
+  keypad(stdscr, true); // enable function keys
+  noecho(); // don't echo while we do getch
 
   breakout_t breakout;
   breakout.setup(10, 5, 100, 30);
 
   display_console_t display_console;
-  breakout.display_board(display_console);
-  breakout.display_paddle(display_console);
-
-  [[maybe_unused]] int key = getch();
+  for (bool running = true; running;) {
+    clear();
+    breakout.display_board(display_console);
+    breakout.display_paddle(display_console);
+    refresh();
+    switch (int key = getch(); key) {
+      case KEY_LEFT:
+        breakout.move_paddle_left(1);
+        break;
+      default:
+        break;
+    }
+  }
 
   endwin();
 
