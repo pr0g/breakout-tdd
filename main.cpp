aa9257da0ec8b7aba0adc1154a6caf2ff425a9c2
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-05-02 18:55:59 +0100

    usage - display paddle in console

--- main.cpp
+++ main.cpp
@@ -8,13 +8,15 @@ struct display_console_t : public display_t {
 };
 
 int main(int argc, char** argv) {
-  initscr();
+  initscr(); // start curses mode
+  curs_set(0); // hide cursor
 
   breakout_t breakout;
   breakout.setup(10, 5, 100, 30);
 
   display_console_t display_console;
   breakout.display_board(display_console);
+  breakout.display_paddle(display_console);
 
   [[maybe_unused]] int key = getch();
 
