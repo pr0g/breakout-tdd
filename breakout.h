4c03b559169ecda74b6f8da70dced23357ad21a5
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-05-02 16:45:48 +0100

    green - breakout_t definition provided

---
+++ breakout.h
@@ -0,0 +1,4 @@
+#pragma once
+
+class breakout_t {
+};
--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
+#include "breakout.h"
+
 TEST_CASE("breakout game") {
   breakout_t breakout;
 }
